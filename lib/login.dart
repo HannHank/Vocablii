7fb0dd82e91419759b3794eb4f885dd92d7663b7
HannHank <hanke.johann@gmail.com>
2020-09-29 16:26:20 +0200

    change login look

--- lib/login.dart
+++ lib/login.dart
@@ -6,47 +6,108 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Screen"),
-      ),
       body: Container(
-        child: Column(
+        child: Stack(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(hintText: "Username"),
-                      ),
-                      SizedBox(height: 10),
-                      TextField(
-                        decoration: InputDecoration(hintText: "Password"),
-                      ),
-                      SizedBox(height: 30),
-                      Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
                         width: double.infinity,
-                        child: RaisedButton(
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
+                        margin: EdgeInsets.only(top: 100),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width: double.infinity,
+                              child: Text(
+                                "Login",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textScaleFactor: 2,
+                                // has impact
+                              ),
                             ),
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, Home.route,arguments:{'User':'Franz Biedenmaier'});
-                           },
-                          color: Colors.blue,
-                        ),
-                      )
-                    ],
-                  ),
+                            SizedBox(height: 30),
+                            Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          blurRadius: 8,
+                                          offset: Offset(0, 15),
+                                          color: Colors.grey.withOpacity(.6),
+                                          spreadRadius: -9),
+                                    ]),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      isDense: true,
+                                      counterText: "",
+                                      contentPadding: EdgeInsets.all(10.0),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            new BorderRadius.circular(10.0),
+                                        borderSide: BorderSide.none,
+                                      ),
+                                      hintText: "Name"),
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  maxLength: 20,
+                                  // controller: _locationNameTextController,
+                                )),
+                            SizedBox(height: 30),
+                            Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          blurRadius: 8,
+                                          offset: Offset(0, 15),
+                                          color: Colors.grey.withOpacity(.6),
+                                          spreadRadius: -9),
+                                    ]),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      isDense: true,
+                                      counterText: "",
+                                      contentPadding: EdgeInsets.all(10.0),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              new BorderRadius.circular(10.0),
+                                          borderSide: BorderSide.none),
+                                      hintText: "Email"),
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  maxLength: 20,
+
+                                  // controller: _locationNameTextController,
+                                )),
+                          ],
+                        )),
+                  ],
                 ),
               ),
-            )
+            ),
+            Container(
+              padding: EdgeInsets.all(30),
+              child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: FloatingActionButton.extended(
+                    backgroundColor: Colors.black,
+                    onPressed: () {},
+                    label: Text("далше"),
+                  )),
+            ),
           ],
         ),
       ),
