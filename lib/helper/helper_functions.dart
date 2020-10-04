46c8be12e8674dde545e9c1fb87e7fa9a5fedd5e
johann-su <johann123.js@gmail.com>
2020-10-04 10:19:35 +0200

    added random color and shuffle for vocabulary

---
+++ lib/helper/helper_functions.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+Map shuffle_map(Map items) {
+  var items_shuffeled = {};
+
+  var keys = items.keys.toList()..shuffle(); 
+  for(var k in keys) { 
+    items_shuffeled[k] = items[k];
+    print('$k, ${items[k]}'); 
+  }
+
+  return items_shuffeled;
+}
+
+Color random_color(List<Color> colors, String word) {
+  var random = new Random(word.length);
+  var color = colors[random.nextInt(colors.length)];
+  return color;
+}
