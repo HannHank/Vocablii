cb584e8fd9f161f98b32b126f3ba93fb4c3b0ef3
johann-su <johann123.js@gmail.com>
2020-10-04 08:11:47 +0200

    fixed bug causing list to crash

--- lib/vocabulary.dart
+++ lib/vocabulary.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_stack_card/flutter_stack_card.dart';
+// import 'package:flutter_stack_card/flutter_stack_card.dart';
 import 'package:flutter/material.dart';
 import 'card/card.dart';
 import 'package:Vocablii/login.dart';
