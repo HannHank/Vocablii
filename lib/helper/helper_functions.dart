a053c9a4eab7c935d7c5ead3cce2550519ac0b98
HannHank <hanke.johann@gmail.com>
2020-10-09 22:22:13 +0200

    moved color list

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:Vocablii/vocabulary.dart';
 import '../components/vocCard.dart';
 
-Color get_color(List<Color> colors, String word,Map userStateVoc) {
+  List<Color> colors = [
+    Color(0xff2B969D),
+    Color(0xff2B529D),
+    Color(0xff862B9D),
+    Color(0xff953232),
+    Color(0xff3B7626),
+    Color(0xff328D93),
+  ];
+
+Color get_color(String word,Map userStateVoc) {
 
   Color color;
   if(userStateVoc[word] == "Iknow"){
