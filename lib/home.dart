fd98a17cc2c79efd1a3f868d173307d2a07d6ef0
johann-su <johann123.js@gmail.com>
2020-10-09 18:38:05 +0200

    made status bar black

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:Vocablii/vocabulary.dart';
-import 'package:flutter/material.dart';
-import 'package:Vocablii/login.dart';
 import 'auth/auth.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
